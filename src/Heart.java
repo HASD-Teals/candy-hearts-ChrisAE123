abad1a1f7fd83cfe6d44a47a5d6a501e8042fe97
ChrisAE123 <magnuschase145@gmail.com>
2022-02-01 09:48:53 -0600

    Overview Changes

--- src/Heart.java
+++ src/Heart.java
@@ -1,32 +1,108 @@
 public class Heart {
     // PROPERTIES
     private String color;
-    private String sizeSayings;
-    private String Calories;
-    private String Cost;
-    private String Availability;
+    private String sayings;
+    private int size;
+    private int calories;
+    private int stock;
+    private double cost;
+    private boolean available;
+ 
     //Add remaining heart properties here...
 
-
     // CONSTRUCTORS
-    public Heart() {} // This is the default contructor
+    public Heart() {
+        this.color="";
+        this.sayings="";
+        this.size=1;
+        this.calories=0;
+        this.stock=0;
+        this.cost=0.00;
+        this.available=false;
+   
+    // This is the default contructor
     //Add overloaded Constructor here...
 
+    } 
+    public void Hearts(String color, String sayings, int size, int calories, int stock, double cost, boolean available){
+    this.color=color;
+    this.sayings=sayings;
+    this.size=size;
+    this.cost=cost;
+    this.stock=stock;
+    if(this.stock>0){
+        this.available=true;
+    }
+    }
 
-    // ACCESSORS
+    // ACCESSOR METHODS
     public String getColor() {
         return this.color;
     }
+    public String getSayings(){
+        return this.sayings;
+    }
+    public int getSize(){
+        return this.size;
+    }
+    public int getCalories(){
+        return this.calories;
+    }
+    public int getStock(){
+        return this.stock;
+    }
+    public double getCost(){
+        return this.cost;
+    }
+    public boolean getAvailable(){
+        return this.available;
+    }
+
+    public String getReport(){
+        String report="Color: "+ this.color + "\n"+"Sayings: "+ this.sayings + "\n"+"Size: "+ this.size + "\n"+"Calories: "+ this.calories + "\n"+"Stock: "+ this.stock + "\n"+"Cost: "+ this.cost + "\n"+"Available: "+ this.available + "\n"; 
+        return report;
+    }
+
     //Add remaining Accessor methods here...
 
+    // MUTATOR METHODS
+    public void adjPrice(double percentage){
+        this.cost=this.cost+(this.cost*percentage/100);
+    }
+    public void inStock(int stock){
+        this.stock=stock;
+        if(this.stock>0){
+            this.available=true;
 
-    // MUTATORS
+        }
+    }
+
+
+
+/*
     public void setColor(String color) {
         this.color = color;
     }
+    public void setSayings(String sayings) {
+        this.sayings=sayings;
+    }
+    public void setSize(int size) {
+        this.size=size;
+    }
+     public void setCalories(int calories) {
+        this.
+    }
+    public void setStock(int stock) {
+        this.
+    }
+    public void setCost(double cost) {
+        this.
+    }
+    public void setAvailable(boolean available) {
+         this.
+    }
     //Add remaining Mutator methods here...
-
-
+*/
     // METHODS
     public String report() {
         return this.color + "\n"; // Add other properties here as you add them to the class
